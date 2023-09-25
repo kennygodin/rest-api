550acdaa20e1beed35ebe7a6df29034526159a0c
kennycodin <kenechukwuokoh30@gmail.com>
2023-09-25 04:08:56 +0100

    Added mongoose and mongoose connections. Created .env file to add mongoURL, created /db and user.ts model

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -22,3 +26,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log('Server running on http://localhost:8080/');
 });
+
+console.log(process.env.MONGO_URL);
+
+// mongoose.Promise = Promise;
+// mongoose.connect(process.env.MONGO_URL);
+// mongoose.connection.on('error', (error: Error) => console.log(error));
